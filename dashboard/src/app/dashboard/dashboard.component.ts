b3e44cb202cacf115710cfe6deca1ce5f40c189a
SamilaOliveira <samilacampelo@gmail.com>
2023-02-10 17:13:47 -0300

    Implementação dos gráficos no componente.

---
+++ dashboard/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { DadosService } from './dados.service';
+import { Observable } from 'rxjs';
+
+declare var google: any;
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+}
